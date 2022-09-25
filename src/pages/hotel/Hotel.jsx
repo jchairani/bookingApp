742758fab5e11f6ad3fe1d8c47bcfa61d7ff6f40
jchairani <chairanijoshua@gmail.com>
2022-09-24 20:47:52 -0700

    Third Commit

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -14,6 +14,8 @@ import { useEffect, useState } from "react";
 import { useLocation } from 'react-router-dom';
 import useFetch from '../../hooks/useFetch';
 import axios from "axios";
+import { useContext } from "react";
+import { SearchContext } from "../../context/SearchContext";
 
 const Hotel = () => {
   //this is location hook... used to get link path
@@ -40,6 +42,18 @@ const Hotel = () => {
   useEffect(()=>{
     fetchData(id);
   },[id])
+
+  const {dates,options} = useContext(SearchContext);
+  console.log(options);
+  // const {dates} = useContext(SearchContext);
+  
+  const MILLISEONCDS_PER_DAY = 1000*60*60*24;
+  function dayDifference(date1,date2){
+    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    const diffDays = Math.ceil(timeDiff / MILLISEONCDS_PER_DAY);
+    return diffDays;
+  }
+  const days = dayDifference(dates[0]?.endDate,dates[0]?.startDate);
 
   const handleOpen = (i) => {
     setSlideNumber(i);
@@ -122,13 +136,13 @@ const Hotel = () => {
               </p>
             </div>
             <div className="hotelDetailsPrice">
-              <h1>Perfect for a 9-night stay!</h1>
+              <h1>Perfect for a {days}-night stay!</h1>
               <span>
                 Located in the real heart of Krakow, this property has an
                 excellent location score of 9.8!
               </span>
               <h2>
-                <b>$945</b> (9 nights)
+                <b>${days * data.cheapestPrice}</b> ({days} nights)
               </h2>
               <button>Reserve or Book Now!</button>
             </div>
